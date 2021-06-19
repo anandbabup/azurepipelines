69e98040690d59c55a1b62c15253fe418a324a42
Anand Babu <Anand.Babu@medpt.com>
2021-06-19 11:49:38 +0530

    New Pipeline added

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = "Anand babu's Todo";
+  title = "Pancharathinam Anand babu's Todo";
 }
